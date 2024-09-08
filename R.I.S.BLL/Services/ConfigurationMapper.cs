2dcae01511f07c7c5fcc99da17db0583d689c794
KazzModan <kklopov64@gmail.com>
2024-09-09 00:40:08 +0300

    Added configuration mapper

---
+++ R.I.S.BLL/Services/ConfigurationMapper.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using R.I.S.DAL.Models;
+using R.I.S.BLL.DTO;
+namespace R.I.S.BLL.Services
+{
+    public class ConfigurationMapper : Profile
+    {
+        public ConfigurationMapper() 
+        {
+            CreateMap<Brand,BrandDTO>().ReverseMap();
+            CreateMap<Category,CategoryDTO>().ReverseMap();
+            CreateMap<Product,ProductDTO>().ReverseMap();
+            CreateMap<Review,ReviewDTO>().ReverseMap();
+            CreateMap<User,UserDTO>().ReverseMap();
+        }   
+    }
+}
